8c42aa5fdde7a4557eaa5ac52479244d437fd72b
hungphan <colorvisavn@gmail.com>
2020-12-17 11:22:17 -0500

    Add spring project

---
+++ src/main/java/com/colorvisavn/springcheatsheet/SpringCheatsheetApplication.java
@@ -0,0 +1,13 @@
+package com.colorvisavn.springcheatsheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCheatsheetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCheatsheetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/colorvisavn/springcheatsheet/SpringCheatsheetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colorvisavn.springcheatsheet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCheatsheetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
